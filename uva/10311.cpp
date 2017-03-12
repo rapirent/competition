0254e9650c83a54c10fc56591452317752ddf5fc
rapirent <rapirent@gmail.com>
2017-03-12 14:43:47 +0800

    10311 solved

--- uva/10311.cpp
+++ uva/10311.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <cmath>
 #include <cstring>
-#define MAX 1000000
+#define MAX 100000000
 
 
 using namespace std;
@@ -12,12 +12,14 @@ bool prime[MAX] = {0};
 void gogoPrime() {
     memset(prime, 1, sizeof(prime));
     for(int i = 2;i <= sqrt(MAX);i++) {
-        if (prime[i]==1) {
+        if (prime[i]==true) {
             for(int j = i*i; j<MAX;j=j+i) {
-                prime[j] = 0;
+                prime[j] = false;
             }
         }
     }
+    prime[0] = false;
+    prime[1] = false;
 }
 
 int main(void)
@@ -26,7 +28,7 @@ int main(void)
     gogoPrime();
     while(scanf("%d",&input)!=EOF) {
         if(input%2) {//奇數
-            if(prime[input-2]==1) {
+            if(prime[input-2]==true) {
                 printf("%d is the sum of %d and %d.\n",input,2,input-2);
             }
             else {
@@ -35,8 +37,8 @@ int main(void)
         }
         else {
             int founded=0;
-            for(int count=input/2;count>=0;count--) {
-                if(prime[count]==1&&prime[input-count]==1) {
+            for(int count=input/2-1;count>=0;count--) {
+                if(prime[count]==true&&prime[input-count]==true) {
                     printf("%d is the sum of %d and %d.\n",input,count,input-count);
                     founded = 1;
                     break;
