332bbed280c8eb364391204ab12912ab488cc1dc
rapirent <rapirent@gmail.com>
2017-03-21 02:47:48 +0800

    solved uva906

--- uva/906.cpp
+++ uva/906.cpp
@@ -8,21 +8,17 @@ using namespace std;
 int main(void)
 {
     const double epsilon = 1e-8;
-    int a,b;
-    while(scanf("%d %d",&a,&b)!=EOF) {
+    long long int a,b;
+    while(scanf("%lld %lld",&a,&b)!=EOF) {
         double eps;
         scanf("%lf",&eps);
-        long double a_divide_b = (long double)a/(long double)b;
-        int c,d=0;
+        long long int c,d=0;
         do{
             d++;
-            c = 1;
-            //我試過用a/b < c/d 跟epsilon比較, 無效 QQ
-            while (a*d >= c*b) {
-                c++;
-            }
-        }while(a_divide_b - (long double)c / (long double)d <= eps);
-        printf("%d %d\n",c,d);
+            c = (long double)a/(long double)b*d;
+            while(a*d>=b*c) ++c;
+        }while( ((long double)c/(long double)d - (long double)a / (long double)b ) > eps);
+        printf("%lld %lld\n",c,d);
     }
     return 0;
 }
