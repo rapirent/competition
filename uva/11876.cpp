b03ffa64fa10472e52352e9dbefabb11128260ab
rapirent <rapirent@gmail.com>
2017-04-06 22:32:58 +0800

    edit README about 714 uva

--- uva/11876.cpp
+++ uva/11876.cpp
@@ -6,17 +6,17 @@
 using namespace std;
 
 int NOD[1000100] = {0};
+int n[1000100] = {0};
 int main(void)
 {
     for(int i = 1;i<1000100;i++) {
-        for(int tmp = i;tmp<100100;tmp+=i) {
+        for(int tmp = i;tmp<1000100;tmp+=i) {
             NOD[i]++;
         }
     }
-    vector<int> n;
     n[0] = 1;
-    for(int i = 1;i<100100;i++) {
-        n.push_back(n.back() + NOD[n.back()]);
+    for(int i = 1;i<1000100;i++) {
+        n[i] = n[i-1] + NOD[n[i-1]];
     }
     int input,input2;
     int repeat;
@@ -24,7 +24,9 @@ int main(void)
     int num = 1;
     while(repeat--) {
         scanf("%d%d",&input,&input2);
-        printf("Case %d: %ld\n",num++,distance(lower_bound(n.begin(),n.end(), input),upper_bound(n.begin(),n.end(),input2)));
+        int lower = lower_bound(n,n+1000100,input) - n;
+        int upper = upper_bound(n,n+1000100,input2) - n;
+        printf("Case %d: %d\n",num++,upper - lower);
     }
     return 0;
 }
